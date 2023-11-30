8fe42e6c1e76e5d46b4bfd174a3d841368113701
José Gabriel Gruber <development@jgabrielgruber.dev>
2023-11-30 11:22:03 -0300

    Implemented echo server with supervisioned tasks

--- apps/mesa_server/lib/mesa_server/application.ex
+++ apps/mesa_server/lib/mesa_server/application.ex
@@ -1,19 +1,17 @@
 defmodule MesaServer.Application do
-  # See https://hexdocs.pm/elixir/Application.html
-  # for more information on OTP Applications
   @moduledoc false
 
   use Application
 
   @impl true
   def start(_type, _args) do
+    port = String.to_integer(System.get_env("PORT") || "4040")
+
     children = [
-      # Starts a worker by calling: MesaServer.Worker.start_link(arg)
-      # {MesaServer.Worker, arg}
+      {Task.Supervisor, name: MesaServer.TaskSupervisor},
+      Supervisor.child_spec({Task, fn -> MesaServer.accept(port) end}, restart: permanent)
     ]
 
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: MesaServer.Supervisor]
     Supervisor.start_link(children, opts)
   end
